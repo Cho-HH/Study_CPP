00ca1c7ca002552c996335213820be80542cea1f
DESKTOP-OU8UB39\82106 <chh1880@gmail.com>
2022-05-12 02:03:44 +0900

    no message

---
+++ Lab2/Lab2.cpp
@@ -0,0 +1,63 @@
+#include "Lab2.h"
+#include <cstdio>
+#include <iomanip>
+#include <cfloat>
+
+using namespace std;
+
+namespace lab2
+{
+	const int SIZE = 256;
+	void PrintIntegers(std::istream& in, std::ostream& out)
+	{
+		out << setw(12) << right << "oct";
+		out << setw(11) << right << "dec";
+		out << setw(9) << right << "hex" << endl;
+		out << setw(13) << right << " ";
+		out << setw(11) << right << " ";
+		out << setw(8) << endl;
+
+		char str[SIZE] = { 0, };
+		while (true)
+		{
+			in.getline(str, SIZE, ' ');
+			int num = 0;
+			if (sscanf(str, "%d", &num) == 1)
+			{
+				out << setw(12) << right << oct << num;
+				out << setw(11) << right << dec << num;
+				out << setw(9) << right << hex << uppercase << num << endl;
+			}
+			if (in.eof())
+			{
+				break;
+			}
+			in.clear();
+		}
+	}
+
+	void PrintMaxFloat(std::istream& in, std::ostream& out)
+	{
+		char str[SIZE] = { 0, };
+		float maxNum = -FLT_MIN;
+		while (true)
+		{
+			in.getline(str, SIZE, ' ');
+			float num = 0;
+			if (sscanf(str, "%f", &num) == 1)
+			{
+				out << "     " << setw(15) << showpos << internal << showpoint << fixed << setprecision(3) << num << endl;
+				if (maxNum < num)
+				{
+					maxNum = num;
+				}
+			}
+			if (in.eof())
+			{
+				break;
+			}
+			in.clear();
+		}
+		out << setw(5) << left << "max:" << setw(15) << showpos << internal << showpoint << fixed << setprecision(3) << maxNum << endl;
+	}
+}
