8c5f61cdc7acbfae39fee4e88a6d30372c69454b
DESKTOP-OU8UB39\82106 <chh1880@gmail.com>
2022-05-31 02:22:36 +0900

    no message

--- Lab5/RectangleLawn.h
+++ Lab5/RectangleLawn.h
@@ -2,20 +2,24 @@
 #include "IFenceable.h"
 #include "Lawn.h"
 
-class RectangleLawn : public IFenceable, public Lawn
+namespace lab5
 {
-public:
-	RectangleLawn(unsigned int width, unsigned int height);
-	virtual ~RectangleLawn();
+	class RectangleLawn : public IFenceable, public Lawn
+	{
+	public:
+		RectangleLawn(unsigned int width, unsigned int height);
+		virtual ~RectangleLawn();
 
-	virtual unsigned int GetArea() const;
+		virtual unsigned int GetArea() const;
 
-	virtual unsigned int GetMinimumFencesCount() const;
-	virtual unsigned int GetFencePrice(eFenceType fenceType) const;
+		virtual unsigned int GetMinimumFencesCount() const;
+		virtual unsigned int GetFencePrice(eFenceType fenceType) const;
 
-private:
-	unsigned int mWidth;
-	unsigned int mHeight;
-	unsigned int mArea;
-};
+	private:
+		unsigned int mWidth;
+		unsigned int mHeight;
+		unsigned int mArea;
+	};
+}
+
 
